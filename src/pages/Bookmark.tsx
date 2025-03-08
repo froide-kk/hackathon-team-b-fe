8f19417d6ac11416d754c57262138ae97e09749f
froide-t.nishimura <takamasa-nishimura@froide.co.jp>
2025-03-08 13:22:34 +0900

    お気に入りページの作成

--- src/pages/Bookmark.tsx
+++ src/pages/Bookmark.tsx
@@ -1,11 +1,21 @@
-import { Container, Typography } from "@mui/material";
+import { Box, Container, Stack, Typography } from "@mui/material";
+import NewsList from "../components/NewsList";
+import { news } from "../utils/example";
 
 export default function Bookmark() {
   return (
-    <Container>
-      <Typography variant="h4" gutterBottom>
-        Bookmark
-      </Typography>
+    <Container maxWidth="xl" sx={{ pt: 8 }}>
+      <Box py={2}>
+        <Stack
+          direction="row"
+          justifyContent="space-between"
+          alignItems="center"
+          padding={2}
+        >
+          <Typography variant="h5">お気に入り</Typography>
+        </Stack>
+      </Box>
+      <NewsList news={news} page="Bookmark" />
     </Container>
   );
 }
